80fee0ae6687f7d16330594fe41aca959c2edce5
k-kikuchi <k-kikuchi@ac-tec.co.jp>
2025-06-02 18:53:17 +0900

    【塚本、菊地】社員選択画面を修正

--- src/components/tables/EmployeeTable.jsx
+++ src/components/tables/EmployeeTable.jsx
@@ -1,68 +1,39 @@
-import React, { Component } from 'react';
 import { Table, Button } from 'reactstrap';
-import EmployeeAddEditModal from '../modals/EmployeeAddEditModal';
 
-class EmployeeTable extends Component {
+const EmployeeTable = () => {
+  let items;
+  if (this.props.items.length > 0) {
+    items = this.props.items.map((item) => {
+      return (
+        <tr key={item.employeeId}>
+          <th scope="row">{item.employeeId}</th>
+          <td>{item.name}</td>
+          <td>{item.mail}</td>
+          <td>
+            <div style={{ margin: 'auto' }}>
 
-  deleteItem = emp_id => {
-    let confirmDelete = window.confirm('削除しますか？');
-    if (confirmDelete) {
-      fetch(`${process.env.REACT_APP_API_ROOT}/employee/delete`, {
-        method: 'delete',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-          emp_id
-        })
-      })
-        .then(response => response.json())
-        .then(item => {
-          this.props.deleteItemFromState(emp_id)
-        })
-        .catch(err => console.log(err));
-    }
+            </div>
+          </td>
+        </tr>
+      );
+      });
+  } else {
+    items = '';
   }
 
-  render() {
-    let items;
-    if (this.props.items.length > 0) {
-      items = this.props.items.map(item => {
-        return (
-          <tr key={item.emp_id}>
-            <th scope="row">{item.emp_id}</th>
-            <td>{item.emp_lname + ' ' + item.emp_fname}</td>
-            <td>{item.mail_address}</td>
-            <td>
-              <div style={{ margin: "auto" }}>
-                <EmployeeAddEditModal mode="編集" item={item} updateState={this.props.updateState} />
-                {' '}
-                <Button color="danger" onClick={() => this.deleteItem(item.emp_id)}>削除</Button>
-              </div>
-            </td>
-          </tr>
-        );
-      })
-    } else {
-      items = '';
-    }
-
-    return (
-      <Table responsive hover>
-        <thead>
-          <tr>
-            <th>社員ID</th>
-            <th>氏名</th>
-            <th>メールアドレス</th>
-            <th></th>
-          </tr>
-        </thead>
-        <tbody>
-          {items}
-        </tbody>
-      </Table>
-    )
-  }
+  return (
+    <Table responsive hover>
+      <thead>
+        <tr>
+          <th>社員ID</th>
+          <th>氏名</th>
+          <th>メールアドレス</th>
+          <th></th>
+        </tr>
+      </thead>
+      <tbody>{items}</tbody>
+    </Table>
+  );
 }
 
 export default EmployeeTable;
